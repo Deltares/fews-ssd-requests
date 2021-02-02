07640dbda472abd3a3ace500fb879387d70bf46b
Ruurd Dorsman <ruurd.dorsman@vortech.nl>
2021-02-02 13:47:33 +0100

    added test for capabilities url

--- test/SsdWebserviceProvider.spec.ts
+++ test/SsdWebserviceProvider.spec.ts
@@ -1,21 +1,28 @@
-import {SsdWebserviceProvider} from '../src/SsdWebserviceProvider'
+import {SsdWebserviceProvider} from '../src/SsdWebserviceProvider';
 
+const baseUrl = "https://rwsos.webservices.deltares.nl/iwp/";
+const apiEndpoint = "FewsWebServices/ssd/";
 const exclude = {
   displayGroups: []
-}
-const apiEndpoint = "FewsWebServices/ssd/"
+};
 
 describe("ssd", function() {
-  it("stores the base url on creation", function() {
-    const url = "https://rwsos.webservices.deltares.nl/iwp/";
-    const provider = new SsdWebserviceProvider(url, exclude);
-    expect(provider.getUrl()).toEqual(url + apiEndpoint);
+  it("creates the api url on creation", function() {
+    const provider = new SsdWebserviceProvider(baseUrl, exclude);
+    expect(provider.getUrl()).toEqual(baseUrl + apiEndpoint);
   });
 
-  it("adds a trailing slash to the base url on creation", function() {
+  it("adds a trailing slash to the supplied url on creation", function() {
     const url = "https://rwsos.webservices.deltares.nl/iwp";
     const provider = new SsdWebserviceProvider(url, exclude);
     expect(provider.getUrl()).toEqual(url + '/' + apiEndpoint);
+  });
+
+  it("gives the correct url to the capabilities", function() {
+    const provider = new SsdWebserviceProvider(baseUrl, exclude);
+    const url = provider.urlForCapabilities();
+    const expected = baseUrl + apiEndpoint + "ssd?request=GetCapabilities&format=application/json";
+    expect(url).toEqual(expected);
   });
 });
 
