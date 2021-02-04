ff02f3d4c67f4d700b02332001446ce9629a92ad
Ruurd Dorsman <ruurd.dorsman@vortech.nl>
2021-02-04 11:03:09 +0100

    added test for retrieving capabilities

--- test/SsdWebserviceProvider.spec.ts
+++ test/SsdWebserviceProvider.spec.ts
@@ -42,5 +42,33 @@ describe("ssd", function() {
     const expected = baseUrl + apiEndpoint + "?request=GetAction&ssd=" + panelId + "&action=leftsingleclick&objectid=" + objectId + "&format=application/json";
     expect(url).toEqual(expected);
   });
+
+  it("retrieves capabilities", async function() {
+    const provider = new SsdWebserviceProvider(baseUrl, exclude);
+    const promise = provider.getCapabilities();
+    const capabilities = await promise;
+
+    const dimensionFormat = {
+      name: expect.any(String),
+      units: expect.any(String),
+      default: expect.any(String),
+      period: expect.any(String)
+    };
+    const displayPanelFormat = {
+      name: expect.any(String),
+      title: expect.any(String),
+      dimension: expect.objectContaining(dimensionFormat)
+    };
+    const displayGroupFormat = {
+      name: expect.any(String),
+      title: expect.any(String),
+      displayPanels: expect.arrayContaining([expect.objectContaining(displayPanelFormat)])
+    };
+    const capabilitiesFormat = {
+      title: expect.any(String),
+      displayGroups: expect.arrayContaining([expect.objectContaining(displayGroupFormat)])
+    };
+    expect(capabilities).toMatchObject(capabilitiesFormat)
+  });
 });
 
