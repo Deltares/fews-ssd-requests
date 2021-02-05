e2c2d73304c8809d43a0b5fe353deaa9dacbcb37
Ruurd Dorsman <ruurd.dorsman@vortech.nl>
2021-02-05 12:21:53 +0100

    simplified webUtils.ts

--- src/utils/webUtils.ts
+++ src/utils/webUtils.ts
@@ -1,16 +1,20 @@
+/**
+ * Interface for the response of an http request
+ */
 export interface HttpResponse<T> extends Response {
   parsedBody?: T;
 }
 
-async function http<T> ( request: RequestInfo ): Promise<HttpResponse<T>> {
+/**
+ * Retrieve a JSON object over HTTP, the json is parsed and the result is placed in the
+ * 'parsedBody' attribute in the response
+ */
+export async function getJsonUsingHttp<T> ( path: string, args: RequestInit = { method: 'get' } ): Promise<HttpResponse<T>> {
+  const request = new Request(path, args)
   const response: HttpResponse<T> = await fetch(
     request
   )
   response.parsedBody = await response.json()
   return response
-}
-
-export async function getJsonUsingHttp<T> ( path: string, args: RequestInit = { method: 'get' } ): Promise<HttpResponse<T>> {
-  return http<T>(new Request(path, args))
 }
 
