0dbc707918d1ea08996d0a322dbe0fd522b16de6
Werner Kramer <werner.kramer@vortech.nl>
2022-03-29 13:23:00 +0200

    Use methods from '@deltares/fews-ssd-request'

--- packages/fews-ssd-requests/src/utils/addLeftClickAction.ts
+++ packages/fews-ssd-requests/src/utils/addLeftClickAction.ts
@@ -9,17 +9,15 @@ export interface ClickCallbackFunction {
  * @param svg the SVG element
  * @param clickCallback the function to be called when the element is clicked
  */
-export function addLeftClickAction (svg: SVGElement, clickCallback: ClickCallbackFunction): void {
-  svg.querySelectorAll('*').forEach(function(el: Element) {
+export function addClickAction(svg: SVGElement, callback: ClickCallbackFunction): void {
+  svg.querySelectorAll<SVGElement>('*').forEach(function (el: SVGElement) {
     const style = el.getAttribute('style') || ""
     if (el.hasAttributeNS(FEWS_NAMESPACE, 'click')) {
-      el.addEventListener('click', function (event: Event) {
-        clickCallback(event)
-      })
       el.setAttribute('style', 'cursor: pointer;' + style)
+      el.setAttribute('pointer-events', 'auto')
+      el.addEventListener('click', callback)
     } else {
-      // clickable elements get a pointer cursor, the others (like text) get the default cursor
-      el.setAttribute('style', 'cursor: default;' + style)
+      el.style.cursor = 'default'
     }
   })
 }
