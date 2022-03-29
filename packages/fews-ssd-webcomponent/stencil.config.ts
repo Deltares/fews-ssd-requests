e1420f2a01ab67653bffa7db9ebe2678738d5d12
Werner Kramer <werner.kramer@vortech.nl>
2022-03-29 12:31:28 +0200

    Fix depencies, remove angular target

--- packages/fews-ssd-webcomponent/stencil.config.ts
+++ packages/fews-ssd-webcomponent/stencil.config.ts
@@ -1,19 +1,15 @@
 import { Config } from '@stencil/core';
-import { angularOutputTarget as angular } from '@stencil/angular-output-target';
+import typescript from 'rollup-plugin-typescript';
 
 export const config: Config = {
   namespace: 'fews-ssd-webcomponent',
   outputTargets: [
-    angular({
-      componentCorePackage: `@deltares/fews-ssd-webcomponent`,
-      directivesProxyFile: `../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts`
-    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
     {
-      type: 'dist-custom-elements-bundle',
+      type: 'dist-custom-elements',
     },
     {
       type: 'docs-readme',
@@ -23,4 +19,9 @@ export const config: Config = {
       serviceWorker: null, // disable service workers
     },
   ],
+  rollupPlugins: {
+    before: [
+      typescript()
+    ]
+  }
 };
