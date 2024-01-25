96eebb4dea1084531351124bacca4d2a9b504811
Werner Kramer <werner.kramer@vortech.nl>
2024-01-25 11:54:15 +0100

    Add convertDatum and useDisplayUnits (#46)

--- packages/fews-ssd-requests/src/requestbuilder/getUrlForAction.ts
+++ packages/fews-ssd-requests/src/requestbuilder/getUrlForAction.ts
@@ -5,10 +5,13 @@ import { ActionRequest } from "../response/requests/actionRequest.js";
  */
 export function getUrlForAction(actionRequest: ActionRequest): string {
     const clickType = actionRequest.clickType === undefined ? "LEFTSINGLECLICK" : actionRequest.clickType;
-    let request = `?request=GetAction&ssd=${actionRequest.panelId}&action=${clickType}&objectid=${actionRequest.objectId}&format=application/json`;
+    let request = `?request=GetAction&ssd=${actionRequest.panelId}&action=${clickType}&objectid=${actionRequest.objectId}`;
     if (actionRequest.timeZero !== undefined) request += `&timezero=${actionRequest.timeZero}`;
+    if (actionRequest.convertDatum !== undefined) request += `&convertDatum=${actionRequest.convertDatum}`;
+    if (actionRequest.useDisplayUnits !== undefined) request += `&useDisplayUnits=${actionRequest.useDisplayUnits}`;
     if (actionRequest.options !== undefined) request += `&options=${actionRequest.options.join(',')}`;
     if (actionRequest.config !== false) request += `&config=true`;
-    return request;
+    request += '&format=application/json'
+   return request;
 }
 
