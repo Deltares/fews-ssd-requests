cd684129c8e94cae2d5abed6b6d8dc6c3abe1f63
hvangeffen <hunter.vangeffen@vortech.nl>
2025-04-02 11:01:42 +0200

    Add weboc dashboard action

--- packages/fews-ssd-requests/src/response/clickType.ts
+++ packages/fews-ssd-requests/src/response/clickType.ts
@@ -1,6 +1,7 @@
 export const ClickType = {
   LEFTSINGLECLICK: 'LEFTSINGLECLICK',
-  LEFTDOUBLECLICK: 'LEFTDOUBLECLICK'
+  LEFTDOUBLECLICK: 'LEFTDOUBLECLICK',
+  WEBOCDASHBOARD: 'WEBOCDASHBOARD',
 } as const
 
 export type ClickType = keyof typeof ClickType
